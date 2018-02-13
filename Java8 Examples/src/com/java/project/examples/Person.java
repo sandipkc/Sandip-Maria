03fa14c0ca0f4367bb131ffb742855b48c6e1c17
Sandip.Chatterji <sandipchaterji@yahoo.com>
2018-02-13 10:16:35 -0500

    Added java files for optional

---
+++ Java8 Examples/src/com/java/project/examples/Person.java
@@ -0,0 +1,13 @@
+package com.java.project.examples;
+
+import java.util.*;
+
+public class Person {
+
+    private Optional<Car> car;
+
+    public Optional<Car> getCar() {
+        return car;
+    }
+}
+
