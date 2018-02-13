02d59cfe8e863ffc02b3fd653a4b77da45b38999
Sandip.Chatterji <sandipchaterji@yahoo.com>
2018-02-13 09:58:52 -0500

    New java files added

---
+++ Java8 Examples/src/com/java/project/examples/Car.java
@@ -0,0 +1,13 @@
+package com.java.project.examples;
+
+import java.util.*;
+
+public class Car {
+
+    private Optional<Insurance> insurance;
+
+    public Optional<Insurance> getInsurance() {
+        return insurance;
+    }
+}
+
---
+++ Java8 Examples/src/com/java/project/examples/Insurance.java
@@ -0,0 +1,11 @@
+package com.java.project.examples;
+
+public class Insurance {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
+
